c1431211ad2890828fd13e719643406f80ad0ea4
Regan Sarwas <regan_sarwas@nps.gov>
2019-06-04 14:39:06 -0800

    Update (improve) the metadata to html tool

--- Metadata2HTML/Program.cs
+++ Metadata2HTML/Program.cs
@@ -1,19 +1,18 @@
-﻿using System;
+﻿using ESRI.ArcGIS;
+using ESRI.ArcGIS.DataSourcesGDB;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
-using ESRI.ArcGIS;
-using ESRI.ArcGIS.esriSystem;
-using ESRI.ArcGIS.Geodatabase;
-using ESRI.ArcGIS.DataSourcesGDB;
 using System.Xml;
 using System.Xml.Xsl;
 
 /*
- * Opens a *.lyr file from a console app and displays various properties.
- * The layer file can be modified and then saved.  See
- * https://desktop.arcgis.com/en/arcobjects/latest/net/webframe.htm#ILayerFile.htm
- * for methods on the ILayerFile.
+ * Opens a Feature Class in a FGDB and reads the metadata
+ * then transforms it using the Esri Stylesheets to an html file
  */
 
 namespace Metadata2HTML
@@ -23,15 +22,18 @@ namespace Metadata2HTML
         [STAThread]
         private static void Main(string[] args)
         {
+            var sw = new Stopwatch();
+            sw.Start();
+
             AoInitialize license = GetLicense(ProductCode.Desktop, esriLicenseProductCode.esriLicenseProductCodeAdvanced);
             if (license == null) return;
+
+            sw.Stop();
+            Console.WriteLine("Time to get license = {0}", sw.Elapsed);
 
             try
             {
                 // Get Feature Class
-
-                // X:\AKR\Statewide\cultural\AKNetworks.gdb\ARCN                  // internal ArcGIS format
-                // X:\AKR\Statewide\cultural\adminbnd.gdb\AdministrativeBoundary  // internal FGDC format
 
                 string workspacePath = @"X:\AKR\Statewide\cultural\AKNetworks.gdb";
                 string featureName = "ARCN";
@@ -49,6 +51,7 @@ namespace Metadata2HTML
                 featureClassName.Name = featureName;
                 featureClassName.WorkspaceName = (IWorkspaceName)workSpace.FullName;
                 ((IName)featureClassName).Open();
+
                 IMetadata metadata = (IMetadata)featureClassName;
                 IXmlPropertySet2 xmlPropertySet2 = (IXmlPropertySet2)metadata.Metadata;
                 String xmlDoc = xmlPropertySet2.GetXml("");
@@ -60,39 +63,75 @@ namespace Metadata2HTML
 
 
                 // Style Sheet
+
                 var xlstFilePath = @"C:\Program Files (x86)\ArcGIS\Desktop10.5\Metadata\Stylesheets\ArcGIS_ItemDescription.xsl";
                 //var xlstFilePath = @"C:\Program Files (x86)\ArcGIS\Desktop10.5\Metadata\Stylesheets\ArcGIS.xsl";
+                //var xlstFilePath = @"C:\Program Files (x86)\ArcGIS\Desktop10.5\Metadata\Stylesheets\ArcGIS_Imports\XML.xslt";
+                //var xlstFilePath = @"C:\Program Files (x86)\ArcGIS\Desktop10.5\Metadata\Stylesheets\ArcGIS_Imports\FGDC.xslt";  //No good - only works as an included stylesheet
 
-                //XmlReader xlstReader = XmlReader.Create(xlstFilePath, null, new XmlParserContext(baseURI:@"C:\Program Files (x86)\ArcGIS\Desktop10.5\Metadata\Stylesheets"));
+                // Load the stylesheet
+                // Loading also compiles the stylesheet, so there is a large startup cost
+                // If possible, load a stylesheet once if it may be used for multiple transformations.
+                // See https://docs.microsoft.com/en-us/dotnet/standard/data/xml/inputs-to-the-xslcompiledtransform-class
 
-                // XLST Transform - see https://docs.microsoft.com/en-us/dotnet/standard/data/xml/inputs-to-the-xslcompiledtransform-class
-                // parameter is for debugging
-                XslCompiledTransform transform = new XslCompiledTransform(true);
-                //XsltSettings settings = new XsltSettings();
-                //settings.EnableScript = true;
-                var myResolver = new MyXmlUrlResolver();
+                // Debugging:
+                // ** Debugging really slows down the compilation and transformation process, so only use it in testing.**
+                // If you wish to debug the compilation of the XLST file, you need to add true as the first/only
+                // parameter to the XslCompiledTransform constructor.
+                // You will also need to add the following to the app.config to avoid 'Stylesheet is too complex warnings'
+                /*
+                 <configSections>
+                   <sectionGroup name="system.xml">
+                     <section name="xslt" type="System.Xml.XmlConfiguration.XsltConfigSection, System.Xml, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" requirePermission="false"/>
+                   </sectionGroup>
+                 </configSections>
+                 <system.xml>
+                   <xslt limitXPathComplexity="false"/>
+                 </system.xml>  
+                 */
+                sw.Restart();
+                Console.WriteLine("Loading/compiling the XLST code");
+                var transform = new XslCompiledTransform();
+                transform.Load(xlstFilePath, null, new XmlUrlResolver());
+                sw.Stop();
+                Console.WriteLine("Loaded/compiled in {0}", sw.Elapsed);
+
+                // Custom Esri extensions are needed to process the Esri Stylesheets
+                // This is based on examination of the Stylesheets and the Esri Libraries
+                // I could not find this documented by Esri, so it may be subject to change without notice
                 XsltArgumentList xslArg = new XsltArgumentList();
-                //Console.WriteLine($"idTags = {ESRI.ArcGIS.Metadata.Editor.XsltExtensionFunctions.GetResString("idTags")}");
                 var esri = new ESRI.ArcGIS.Metadata.Editor.XsltExtensionFunctions();
                 xslArg.AddExtensionObject("http://www.esri.com/metadata/", esri);
-                //xslArg.AddExtensionObject("http://www.esri.com/metadata/res", esri);
-                Console.WriteLine("Loading/compiling the XLST code");
-                //transform.Load(xlstFilePath, XsltSettings.TrustedXslt, myResolver);
-                //transform.Load(xlstFilePath, settings, myResolver);
-                transform.Load(xlstFilePath, null, myResolver);
-                Console.WriteLine("Transforming the XML to html");
+                xslArg.AddExtensionObject("http://www.esri.com/metadata/res/", esri);
+
                 // HTML output
+                Console.WriteLine("Transforming the XML to html");
                 using (TextWriter writer = new Utf8StringWriter())
                 {
                     using (XmlWriter xmlWriter = XmlWriter.Create(writer))
                     {
+                        sw.Restart();
                         transform.Transform(xmlReader, xslArg, xmlWriter);
+                        sw.Stop();
+                        Console.WriteLine("Transformed in {0}", sw.Elapsed);
                     }
+                    //File.WriteAllText(@"C:\tmp\metadata_in.html", writer.ToString());
+
                     // Replace the localizable elements <res:xxx /> with the localized text
-                    var pattern = @"<res:(\w+)\s*/>";
-                    MatchEvaluator evaluator = EsriLocalizer;
-                    var htmlText = Regex.Replace(writer.ToString(), pattern, evaluator);
-                    System.IO.File.WriteAllText(@"C:\tmp\metadata.html", htmlText);
+                    var pattern = @"<res:(\w+)(?:\s?|\s\S*\s)/>";
+                    //var pattern = @"<res:(\w+)\s?/>";
+                    MatchEvaluator evaluator = EsriLocalize;
+                    sw.Restart();
+                    var htmlText = Regex.Replace(writer.ToString(), pattern, evaluator, RegexOptions.None, TimeSpan.FromSeconds(0.25));
+                    sw.Stop();
+                    Console.WriteLine("Localized HTML in {0}", sw.Elapsed);
+                    //Add DOCTYPE
+                    htmlText = htmlText.Replace("<?xml version=\"1.0\" encoding=\"utf-8\"?>", 
+                        "<?xml version=\"1.0\" encoding=\"utf-8\"?><!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
+                    //Fix Thumbnail centering
+                    htmlText = htmlText.Replace(".noThumbnail {",
+                        ".noThumbnail {display:inline-block;");
+                    File.WriteAllText(@"C:\tmp\metadata.html", htmlText);
                 }
 
                 Shutdown(license, "Successful Completion");
@@ -104,7 +143,7 @@ namespace Metadata2HTML
             }
         }
 
-        private static string EsriLocalizer(Match match)
+        private static string EsriLocalize(Match match)
         {
             return ESRI.ArcGIS.Metadata.Editor.XsltExtensionFunctions.GetResString(match.Groups[1].Value);
         }
@@ -142,22 +181,9 @@ namespace Metadata2HTML
         }
     }
 
-    class MyXmlUrlResolver : XmlUrlResolver
-    {
-        public override Uri ResolveUri(Uri baseUri, string relativeUri)
-        {
-            if (baseUri != null)
-                return base.ResolveUri(baseUri, relativeUri);
-            else
-                return base.ResolveUri(new Uri(@"file://C:\Program Files(x86)\ArcGIS\Desktop10.5\Metadata\Stylesheets"), relativeUri);
-        }
-    }
-
+    // The default string writer is encodes UTF16 which is not compatible with my UTF8 XML
     public class Utf8StringWriter : StringWriter
     {
-        public override Encoding Encoding
-        {
-            get { return Encoding.UTF8; }
-        }
+        public override Encoding Encoding => Encoding.UTF8;
     }
 }
